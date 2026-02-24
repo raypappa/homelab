367c7781a443dd179c954f880198ae813b7fa572
raypappa <kg5ohk@gmail.com>
2026-02-23 21:00:01 -0800

    fix(pulumi): weasel config settings

--- pulumi/stacks/weasel.py
+++ pulumi/stacks/weasel.py
@@ -3,29 +3,25 @@ from pulumi_aws import route53
 
 
 class Weasel(pulumi.ComponentResource):
-    def __init__(self, name, primary_hosted_zone_name: str, opts = None):
-        super().__init__('pkg:index:Weasel', name, None, opts)
+    def __init__(self, name, primary_hosted_zone_name: str, opts=None):
+        super().__init__("pkg:index:Weasel", name, None, opts)
         selected = route53.get_zone(
             name=primary_hosted_zone_name,
             private_zone=False,
         )
         route53.Record(
-            'weasel_A_record',
+            "weasel_A_record",
             zone_id=selected.zone_id,
-            name=f'weasel.{selected.name}',
+            name=f"weasel.{selected.name}",
             type=route53.RecordType.A,
             ttl=86400,
-            records=[
-                pulumi.Config().require('weasel:primary_ipv4')
-            ]
+            records=[pulumi.Config().require_object("weasel")["primary_ipv4"]],
         )
         route53.Record(
-            'weasel_AAAA_record',
+            "weasel_AAAA_record",
             zone_id=selected.zone_id,
-            name=f'weasel.{selected.name}',
+            name=f"weasel.{selected.name}",
             type=route53.RecordType.AAAA,
             ttl=86400,
-            records=[
-                pulumi.Config().require('weasel:primary_ipv6')
-            ]
+            records=[pulumi.Config().require_object("weasel")["primary_ipv6"]],
         )
